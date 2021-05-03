3bc5bcdccb822571285c9a008ade7addc65d13f6
ESolovian <geosha-sol@yandex.ru>
2021-05-04 00:08:56 +0300

    Сделано

--- src/main/java/problem/Rectangle.java
+++ src/main/java/problem/Rectangle.java
@@ -29,14 +29,16 @@ public class Rectangle {
         Random r = new Random();
         double nx1 = (double) r.nextInt(50) / 25 - 1;
         double ny1 = (double) r.nextInt(50) / 25 - 1;
-        double nx3 = (double) r.nextInt(50) / 25 - 1;
-        double ny3 = (double) r.nextInt(50) / 25 - 1;
+        double nx3 = nx1;
+        while(nx1 == nx3) nx3 = (double) r.nextInt(50) / 25 - 1;
+        double ny3 = ny1;
+        while(ny1 == ny3) ny3 = (double) r.nextInt(50) / 25 - 1;
         return new Rectangle(nx1, ny1, nx3, ny3);
     }
     public void render(GL2 gl) {
-        gl.glPointSize(3);
+        gl.glLineWidth(3);
+        gl.glColor3d(0.0, 1.0, 0.0);
         gl.glBegin(GL.GL_LINES);
-        gl.glColor3d(0.0, 0.0, 1.0);
         gl.glVertex2d(x1, y1);
         gl.glVertex2d(x2, y2);
         gl.glVertex2d(x2, y2);
@@ -46,5 +48,6 @@ public class Rectangle {
         gl.glVertex2d(x4, y4);
         gl.glVertex2d(x1, y1);
         gl.glEnd();
+        gl.glPointSize(1);
     }
 }
